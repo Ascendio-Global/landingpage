ca5f25bc21552c6dc06cd864f31d3b6b4b47f186
Jess <jessmathews190@gmail.com>
2026-05-31 19:12:28 +0530

    - remove gh-pages config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/landingpage',
-  output: "export",
-  // assetPrefix: './',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
